d77431baeb11e3487edae185cec6bda3523ca3e8
Mend1s <andremendesskt99@gmail.com>
2025-05-12 19:09:34 -0300

    feat: add material e ajustando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PessoasComponent } from '../pages/pessoas/pessoas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list-person',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list-person',
+    component: PessoasComponent
+  }
+];
