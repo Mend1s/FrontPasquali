e6e67ede182b0eb70bf356a7b9c1b57b5ca5cc80
Mend1s <andremendesskt99@gmail.com>
2025-05-12 19:52:21 -0300

    feat: implement pessoas component with data table and filtering

--- src/services/pessoa.service.ts
+++ src/services/pessoa.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pessoa } from '../models/pessoa.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService {
+  private apiUrl = 'https://localhost:7018/Pessoas';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  obterPessoas(): Observable<Pessoa[]> {
+    return this.http.get<Pessoa[]>(this.apiUrl);
+  }
 }
